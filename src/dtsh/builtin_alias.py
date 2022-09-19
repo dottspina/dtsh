c95c7785158906aef7ddca66cc74a7b10dc8202b
Chris Duf <chris@openmarl.org>
2022-09-19 18:58:43 +0200

    alias: fix autocomp, support pager and rich output

--- src/dtsh/builtin_alias.py
+++ src/dtsh/builtin_alias.py
@@ -2,16 +2,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""Built-in 'pwd' command."""
+"""Built-in 'alias' command."""
 
 
-from dtsh.dtsh import Dtsh, DtshCommand, DtshVt
-from dtsh.dtsh import DtshCommandUsageError
+from typing import Tuple
+
+from rich.table import Table
+
+from dtsh.dtsh import Dtsh, DtshCommand, DtshCommandOption, DtshAutocomp, DtshVt
+from dtsh.dtsh import DtshError, DtshCommandUsageError
 from dtsh.tui import DtshTui
 
 
 class DtshBuiltinAlias(DtshCommand):
-    """Print current working node's path.
+    """Print defined aliases.
 
 DESCRIPTION
 The `alias` command prints the aliases defined
@@ -36,31 +40,78 @@ mcuboot-button0 → /buttons/button_0
 mcuboot-led0    → /leds/led_0
 watchdog0       → /soc/watchdog@40010000         
 spi-flash0      → /soc/qspi@40029000/mx25r6435f@0
+
+/
+❯ alias watchdog0 -l
+watchdog0 → /soc/watchdog@40010000 nordic,nrf-wdt
 ```
 """
     def __init__(self, shell: Dtsh):
         super().__init__(
             'alias',
-            "print defined aliases"
+            "print defined aliases",
+            True,
+            [
+                DtshCommandOption('use rich listing format', 'l', None, None),
+            ]
         )
         self._dtsh = shell
+
+    @property
+    def usage(self) -> str:
+        """Overrides DtshCommand.usage().
+        """
+        return super().usage + ' [ALIAS]'
+
+    @property
+    def with_rich_fmt(self) -> bool:
+        return self.with_flag('-l')
 
     def parse_argv(self, argv: list[str]) -> None:
         """Overrides DtshCommand.parse_argv().
         """
         super().parse_argv(argv)
 
-    def execute(self, stdout: DtshVt) -> None:
+    def execute(self, vt: DtshVt) -> None:
         """Implements DtshCommand.execute().
         """
         if self.with_usage_summary:
-            stdout.write(self.usage)
+            vt.write(self.usage)
             return
-        if len(self._params) > 0:
+        if len(self._params) > 1:
             raise DtshCommandUsageError(self, 'too many parameters')
 
-        view = DtshTui.mk_grid(3)
-        for alias, node in self._dtsh.dt_aliases.items():
+        if self._params:
+            arg_aliases = [self._params[0]]
+        else:
+            arg_aliases = list(self._dtsh.dt_aliases.keys())
+
+        if self.with_pager:
+            vt.pager_enter()
+        if self.with_rich_fmt:
+            grid = self._mk_grid_aliases_rich(arg_aliases)
+        else:
+            grid = self._mk_grid_aliases(arg_aliases)
+        vt.write(grid)
+        if self.with_pager:
+            vt.pager_exit()
+
+    def autocomplete_param(self, prefix: str) -> Tuple[int,list]:
+        """Overrides DtshCommand.autocomplete_param().
+        """
+        completions = list[str]()
+        for alias in self._dtsh.dt_aliases:
+            if alias.startswith(prefix) and (len(alias) > len(prefix)):
+                completions.append(alias)
+        return (DtshAutocomp.MODE_ANY, completions)
+
+    def _mk_grid_aliases(self, arg_aliases: list[str]) -> Table:
+        grid = DtshTui.mk_grid(3)
+        for alias in arg_aliases:
+            try:
+                node = self._dtsh.dt_aliases[alias]
+            except KeyError:
+                raise DtshError(f'no such alias: {arg_aliases[0]}')
             txt_alias = DtshTui.mk_txt(alias)
             txt_arrow = DtshTui.mk_txt(DtshTui.WCHAR_ARROW)
             txt_path = DtshTui.mk_txt(node.path)
@@ -68,5 +119,23 @@ spi-flash0      → /soc/qspi@40029000/mx25r6435f@0
                 DtshTui.txt_dim(txt_alias)
                 DtshTui.txt_dim(txt_arrow)
                 DtshTui.txt_dim(txt_path)
-            view.add_row(txt_alias, txt_arrow, txt_path)
-        stdout.write(view)
+            grid.add_row(txt_alias, txt_arrow, txt_path)
+        return grid
+
+    def _mk_grid_aliases_rich(self, arg_aliases: list[str]) -> Table:
+        grid = DtshTui.mk_grid(4)
+        for alias in arg_aliases:
+            try:
+                node = self._dtsh.dt_aliases[alias]
+            except KeyError:
+                raise DtshError(f'no such alias: {arg_aliases[0]}')
+            txt_alias = DtshTui.mk_txt(alias, DtshTui.style(DtshTui.STYLE_DT_ALIAS))
+            txt_arrow = DtshTui.mk_txt(DtshTui.WCHAR_ARROW)
+            txt_path = DtshTui.mk_txt_node_path(node.path)
+            txt_binding = DtshTui.mk_txt_node_binding(node, True, True)
+            if node.status != 'okay':
+                DtshTui.txt_dim(txt_alias)
+                DtshTui.txt_dim(txt_arrow)
+                DtshTui.txt_dim(txt_path)
+            grid.add_row(txt_alias, txt_arrow, txt_path, txt_binding)
+        return grid
