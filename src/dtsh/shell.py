cc06b6b8e150f645241e15818e69b29f53911b15
Chris Duf <chris@openmarl.org>
2022-10-13 23:41:56 +0200

    shell: default bindings should rely on CMake variables when possible
    When no binding directories are provided by the dtsh command line,
    we should try to actually default to the bindings Zephyr would rely on
    at build-time.
    
    "Where are bindings located ?" specifies that binding files are
    expected to be located in the dts/bindings sub-directories of:
    - the zephyr repository
    - the application source directory
    - the board directory
    - any directories in DTS_ROOT
    - any module that defines a dts_root in its build
    
    Walking through the modules' build settings seems a lot of work
    (needs investigation, and confirmation that it's worth the effort),
    but we should at least try include:
    - $ZEPHYR_BASE/dts/bindings
    - APPLICATION_SOURCE_DIR/dts/bindings
    - BOARD_DIR/dts/bindings
    - DTS_ROOT/**/dts/bindings
    
    If that fails, we mainly fallback to the previous behavior:
    - APPLICATION_SOURCE_DIR will default to $PWD
    - we will substitute BOARD_DIR/dts/bindings with the ordered
      list [$ZEPHYR_BASE/boards, $PWD/boards]
    
    The later my be incorrect: may we have multiple YAML binding files
    with the same name, but for different boards (in different directories) ?
    Be that as it may, it seems harmless, see:
    - https://github.com/dottspina/dtsh/issues/1#issuecomment-1278281428

--- src/dtsh/shell.py
+++ src/dtsh/shell.py
@@ -4,10 +4,12 @@
 
 """Devicetree shell PoC implementation."""
 
+from pathlib import Path
 import os
 
 from devicetree.edtlib import EDT
 
+from dtsh.cmake import CMakeHelper
 from dtsh.dtsh import Dtsh, DtshError
 from dtsh.builtin_pwd import DtshBuiltinPwd
 from dtsh.builtin_alias import DtshBuiltinAlias
@@ -71,15 +73,12 @@ class DevicetreeShell(Dtsh):
         if not dt_bindings_path:
             zephyr_base = os.getenv('ZEPHYR_BASE')
             if zephyr_base:
-                dt_bindings_path = [
-                    os.path.join(zephyr_base, 'boards'),
-                    os.path.join(zephyr_base, 'dts', 'bindings')
-                ]
+                dt_bindings_path = DevicetreeShell.get_zephyr_binding_dirs(
+                    zephyr_base,
+                    dt_source_path
+                )
             else:
                 raise DtshError('Please provide DT bindings or set ZEPHYR_BASE.')
-        for path in dt_bindings_path:
-            if not os.path.isdir(path):
-                raise DtshError(f"DT bindings directory not found: {path}")
 
         try:
             edt = EDT(dt_source_path, dt_bindings_path)
@@ -87,3 +86,90 @@ class DevicetreeShell(Dtsh):
             raise DtshError('Devicetree initialization failed.', e)
 
         return DevicetreeShell(edt)
+
+    @staticmethod
+    def get_zephyr_binding_dirs(zephyr_base: str, dts_path:str) -> list[str]:
+        """Answers the list of binding directories Zephyr 'would use'
+        at build-time.
+
+        "Where are bindings located ?" specifies that binding files are
+        expected to be located in dts/bindings sub-directory of:
+        - the zephyr repository
+        - the application source directory
+        - the board directory
+        - any directories in DTS_ROOT
+        - any module that defines a dts_root in its build
+
+        Walking through the modules' build settings seems a lot of work
+        (needs investigation, and confirmation that it's worth the effort),
+        but we should at least try to include:
+        - $ZEPHYR_BASE/dts/bindings
+        - APPLICATION_SOURCE_DIR/dts/bindings
+        - BOARD_DIR/dts/bindings
+        - DTS_ROOT/**/dts/bindings
+
+        This implies we get the value of the CMake cached variables
+        APPLICATION_SOURCE_DIR, BOARD_DIR and DTS_ROOT.
+        To invoke CMake, we'll first need a value for APPLICATION_BINARY_DIR:
+        we'll assume its the parent of the directory containing the DTS file,
+        as in <app_root>/build/zephyr/zephyr.dts.
+
+        If that fails:
+        - APPLICATION_SOURCE_DIR will default to $PWD
+        - we will substitute BOARD_DIR/dts/bindings with the ordered
+          list [$ZEPHYR_BASE/boards, $PWD/boards]
+        - DTS_ROOT: no sensible default
+
+        Only directories that actually exist are included.
+
+        See:
+        - $ZEPHYR_BASE/cmake/modules/dts.cmake
+        - https://docs.zephyrproject.org/latest/build/dts/bindings.html#where-bindings-are-located
+
+        Arguments:
+        zephyr_base -- value of the ZEPHYR_BASE environment variable
+        dts_path -- path to the DTS file
+        """
+        binding_dirs = list[str]()
+        dts_dir = os.path.dirname(dts_path)
+        app_binary_dir = str(Path(dts_dir).parent.absolute())
+        cmake = CMakeHelper(app_binary_dir)
+
+        # $ZEPHYR_BASE/dts/bindings should exist.
+        binding_dirs.append(os.path.join(zephyr_base, 'dts', 'bindings'))
+
+        app_src_dir = cmake.getcache('APPLICATION_SOURCE_DIR')
+        if not app_src_dir:
+            # APPLICATION_SOURCE_DIR will default to $PWD.
+            app_src_dir = os.getcwd()
+        path = os.path.join(app_src_dir, 'dts', 'bindings')
+        if os.path.isdir(path):
+            binding_dirs.append(path)
+
+        board_dir = cmake.getcache('BOARD_DIR')
+        if board_dir:
+            path = os.path.join(board_dir, 'dts', 'bindings')
+            if os.path.isdir(path):
+                binding_dirs.append(path)
+        else:
+            # When BOARD_DIR is unset, we add $ZEPHYR_BASE/boards and $PWD/boards,
+            # instead of BOARD_DIR/dts/bindings.
+            #
+            # ISSUE: may we have multiple YAML binding files with the same name,
+            # but for different boards (in different directories) ?
+            path = os.path.join(os.getcwd(), 'boards')
+            if os.path.isdir(path):
+                binding_dirs.append(path)
+            path = os.path.join(zephyr_base, 'boards')
+            if os.path.isdir(path):
+                binding_dirs.append(path)
+
+        dts_root = cmake.getcache('DTS_ROOT')
+        if dts_root:
+            # Append all DTS_ROOT/**/dts/bindings we find.
+            for root, _, _ in os.walk(dts_root):
+                path = os.path.join(root, 'dts', 'bindings')
+                if os.path.isdir(path):
+                    binding_dirs.append(path)
+
+        return binding_dirs
