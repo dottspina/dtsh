d4efc7849d0bf989482ab9b03f3dc5904e5c6feb
Christophe Dufaza <chris@openmarl.org>
2024-02-20 19:01:57 +0100

    ci: bump version to 0.2.0 stable
    Freeze currently implemented features as 0.2.0 stable,
    dropping the meaningless RC postfix.
    
    Note: this version is now ahead of the RFC content (as of 4009cec),
    which should be updated.

--- src/dtsh/shell.py
+++ src/dtsh/shell.py
@@ -728,7 +728,7 @@ class DTSh:
     - parse command lines and execute command strings
     """
 
-    VERSION_STRING = "0.2rc1"
+    VERSION_STRING = "0.2.0"
     """The devicetree shell version string.
 
     This version identifies:
