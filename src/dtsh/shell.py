96799620d8576c8b218a22144ce3baf4054d68a2
Christophe Dufaza <chris@openmarl.org>
2024-04-12 18:35:11 +0200

    shell: set version to 0.2.2
    Update version string before release.

--- src/dtsh/shell.py
+++ src/dtsh/shell.py
@@ -728,7 +728,7 @@ class DTSh:
     - parse command lines and execute command strings
     """
 
-    VERSION_STRING = "0.2.1"
+    VERSION_STRING = "0.2.2"
     """The devicetree shell version string.
 
     This version identifies:
