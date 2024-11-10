2c773a3dae7c7a2fcdb51c3d1c11955901c6bd90
Christophe Dufaza <chris@openmarl.org>
2024-11-10 01:14:06 +0100

    rich: io: move private method after public API
    While at it:
    - fix a typo
    - remove obsolete and confusing DTSH_SVG_META_FORMAT

--- src/dtsh/rich/io.py
+++ src/dtsh/rich/io.py
@@ -252,22 +252,6 @@ class DTShOutputFileHtml(DTShOutputFile):
         except OSError as e:
             raise DTShRedirect.Error(e.strerror) from e
 
-    def _mk_html_format(self) -> str:
-        font_family = _dtshconf.pref_html_font_family
-        if font_family:
-            font_family = f"{font_family},monospace"
-        else:
-            font_family = "monospace"
-        html_fmt = DTSH_HTML_META_FORMAT.replace("|font_family|", font_family)
-
-        html_fmt = html_fmt.replace(
-            # "medium" is the default (absolute) size.
-            "|font_size|",
-            _dtshconf.pref_html_font_size or "medium",
-        )
-
-        return html_fmt
-
     def write(self, *args: Any, **kwargs: Any) -> None:
         """Capture and record outputs.
 
@@ -350,6 +334,22 @@ class DTShOutputFileHtml(DTShOutputFile):
             print(line, file=self._out)
         self._out.close()
 
+    def _mk_html_format(self) -> str:
+        font_family = _dtshconf.pref_html_font_family
+        if font_family:
+            font_family = f"{font_family},monospace"
+        else:
+            font_family = "monospace"
+        html_fmt = DTSH_HTML_META_FORMAT.replace("|font_family|", font_family)
+
+        html_fmt = html_fmt.replace(
+            # "medium" is the default (absolute) size.
+            "|font_size|",
+            _dtshconf.pref_html_font_size or "medium",
+        )
+
+        return html_fmt
+
     def _seek_last_content(self) -> None:
         # Offset for the point of insertion, just before the HTML epilog.
         offset: int = self._out.tell()
@@ -412,7 +412,7 @@ class DTShOutputFileSVG(DTShOutputFile):
         """
         if self._append and not self._width:
             # When appending to an existing content (output file),
-            # insert a blank line before we start to atually
+            # insert a blank line before we start to actually
             # capture the last command output.
             #
             # NOTE: we can't do that on flush, it will be too late,
@@ -521,63 +521,6 @@ body {{
 
 </html>
 """
-
-# See also: rich._export_format.CONSOLE_SVG_FORMAT
-DTSH_SVG_META_FORMAT = """\
-<svg class="rich-terminal" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">
-    <!-- Generated with Rich https://www.textualize.io -->
-    <style>
-
-    @font-face {{
-        font-family: "Fira Code";
-        src: local("FiraCode-Regular"),
-                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff2/FiraCode-Regular.woff2") format("woff2"),
-                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff/FiraCode-Regular.woff") format("woff");
-        font-style: normal;
-        font-weight: 400;
-    }}
-    @font-face {{
-        font-family: "Fira Code";
-        src: local("FiraCode-Bold"),
-                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff2/FiraCode-Bold.woff2") format("woff2"),
-                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff/FiraCode-Bold.woff") format("woff");
-        font-style: bold;
-        font-weight: 700;
-    }}
-
-    .{unique_id}-matrix {{
-        font-family: |font_family|;
-        font-size: {char_height}px;
-        line-height: {line_height}px;
-        font-variant-east-asian: full-width;
-    }}
-
-    .{unique_id}-title {{
-        font-size: 18px;
-        font-weight: bold;
-        font-family: arial;
-    }}
-
-    {styles}
-    </style>
-
-    <defs>
-    <clipPath id="{unique_id}-clip-terminal">
-      <rect x="0" y="0" width="{terminal_width}" height="{terminal_height}" />
-    </clipPath>
-    {lines}
-    </defs>
-
-    {chrome}
-    <g transform="translate({terminal_x}, {terminal_y})" clip-path="url(#{unique_id}-clip-terminal)">
-    {backgrounds}
-    <g class="{unique_id}-matrix">
-    {matrix}
-    </g>
-    </g>
-</svg>
-"""
-
 
 DTSH_EXPORT_THEMES: Mapping[str, TerminalTheme] = {
     "svg": SVG_EXPORT_THEME,
