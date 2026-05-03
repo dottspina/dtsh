e311d8151118e1a2d1c5ce406fe59e8022e6f9d6
Christophe Dufaza <chris@openmarl.org>
2026-05-03 16:03:21 +0200

    ci: upgrade python-devicetree to Zephyr 4.4.0
    Upgrade the packaged version of the python-devicetree
    library to Zephyr 4.4.0.

--- src/devicetree/dtlib.py
+++ src/devicetree/dtlib.py
@@ -123,7 +123,12 @@ class Node:
         """
         # Converted to a property to discourage renaming -- that has to be done
         # via DT.move_node.
-        return self._name
+        name, sep, address = self._name.partition("@")
+
+        if sep and address:  # means '@' was found
+            return f"{name}@{address.lower()}"
+        else:
+            return name
 
     @property
     def lineno(self) -> int:
@@ -247,7 +252,7 @@ class Node:
         Returns some information about the Node instance. Called automatically
         if the Node instance is evaluated.
         """
-        return f"<Node {self.path} in '{self.dt.filename}'>"
+        return f"<Node {self.path} in {self.filename}:{self.lineno}>"
 
 # See Property.type
 class Type(enum.IntEnum):
@@ -437,7 +442,7 @@ class Property:
         """
         if self.type is not Type.NUM:
             _err(f"expected property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} to be assigned with "
+                 f"{self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = < (number) >;', not '{self}'")
 
         return int.from_bytes(self.value, "big", signed=signed)
@@ -457,7 +462,7 @@ class Property:
         """
         if self.type not in (Type.NUM, Type.NUMS):
             _err(f"expected property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} to be assigned with "
+                 f"{self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = < (number) (number) ... >;', not '{self}'")
 
         return [int.from_bytes(self.value[i:i + 4], "big", signed=signed)
@@ -475,7 +480,7 @@ class Property:
         """
         if self.type is not Type.BYTES:
             _err(f"expected property '{self.name}' on {self.node.path} "
-                 f"in {self.node.dt.filename} to be assigned with "
+                 f"in {self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = [ (byte) (byte) ... ];', not '{self}'")
 
         return self.value
@@ -494,14 +499,14 @@ class Property:
         """
         if self.type is not Type.STRING:
             _err(f"expected property '{self.name}' on {self.node.path} "
-                 f"in {self.node.dt.filename} to be assigned with "
+                 f"in {self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = \"string\";', not '{self}'")
 
         try:
             ret = self.value.decode("utf-8")[:-1]  # Strip null
         except UnicodeDecodeError:
             _err(f"value of property '{self.name}' ({self.value!r}) "
-                 f"on {self.node.path} in {self.node.dt.filename} "
+                 f"on {self.node.path} in {self.filename}:{self.lineno} "
                  "is not valid UTF-8")
 
         return ret  # The separate 'return' appeases the type checker.
@@ -519,14 +524,14 @@ class Property:
         """
         if self.type not in (Type.STRING, Type.STRINGS):
             _err(f"expected property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} to be assigned with "
+                 f"{self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = \"string\", \"string\", ... ;', not '{self}'")
 
         try:
             ret = self.value.decode("utf-8").split("\0")[:-1]
         except UnicodeDecodeError:
             _err(f"value of property '{self.name}' ({self.value!r}) "
-                 f"on {self.node.path} in {self.node.dt.filename} "
+                 f"on {self.node.path} in {self.filename}:{self.lineno} "
                  "is not valid UTF-8")
 
         return ret  # The separate 'return' appeases the type checker.
@@ -542,7 +547,7 @@ class Property:
         """
         if self.type is not Type.PHANDLE:
             _err(f"expected property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} to be assigned with "
+                 f"{self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = < &foo >;', not '{self}'")
 
         return self.node.dt.phandle2node[int.from_bytes(self.value, "big")]
@@ -567,7 +572,7 @@ class Property:
 
         if not type_ok():
             _err(f"expected property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} to be assigned with "
+                 f"{self.filename}:{self.lineno} to be assigned with "
                  f"'{self.name} = < &foo &bar ... >;', not '{self}'")
 
         return [self.node.dt.phandle2node[int.from_bytes(self.value[i:i + 4],
@@ -588,7 +593,7 @@ class Property:
         """
         if self.type not in (Type.PATH, Type.STRING):
             _err(f"expected property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} to be assigned with either "
+                 f"{self.filename}:{self.lineno} to be assigned with either "
                  f"'{self.name} = &foo' or '{self.name} = \"/path/to/node\"', "
                  f"not '{self}'")
 
@@ -596,15 +601,15 @@ class Property:
             path = self.value.decode("utf-8")[:-1]
         except UnicodeDecodeError:
             _err(f"value of property '{self.name}' ({self.value!r}) "
-                 f"on {self.node.path} in {self.node.dt.filename} "
+                 f"on {self.node.path} in {self.filename}:{self.lineno} "
                  "is not valid UTF-8")
 
         try:
             ret = self.node.dt.get_node(path)
         except DTError:
             _err(f"property '{self.name}' on {self.node.path} in "
-                 f"{self.node.dt.filename} points to the non-existent node "
-                 f'"{path}"')
+                 f"{self.filename}:{self.lineno} points to the non-existent "
+                 f'node "{path}"')
 
         return ret  # The separate 'return' appeases the type checker.
 
@@ -678,8 +683,8 @@ class Property:
         return s + ";"
 
     def __repr__(self):
-        return (f"<Property '{self.name}' at '{self.node.path}' in "
-                f"'{self.node.dt.filename}'>")
+        return (f"<Property '{self.name}' on {self.node.path} in "
+                f"{self.filename}:{self.lineno}>")
 
     #
     # Internal functions
@@ -2168,11 +2173,22 @@ def _root_and_path_to_node(cur, path, fullpath):
         if not component:
             continue
 
-        if component not in cur.nodes:
-            _err(f"component '{component}' in path '{fullpath}' "
-                 "does not exist")
+        name, sep, address = component.partition("@")
+        if sep:
+            component = f"{name}@{address.lower()}"
+        else:
+            component = name
 
-        cur = cur.nodes[component]
+        if component not in cur.nodes:
+            prefix = f"{name}@"
+            names = [k for k in cur.nodes if k.startswith(prefix)]
+            if len(names) == 1:
+                cur = cur.nodes[names[0]]
+            else:
+                _err(f"component '{component}' in path '{fullpath}' "
+                     "does not exist")
+        else:
+            cur = cur.nodes[component]
 
     return cur
 
